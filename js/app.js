43ac69186d214301953bd6400b874ec2975c4ea0
Jormangandur <=>
2017-06-24 13:52:46 +0100

    Add Foundation, basic top bar and responsive sidebar

--- js/app.js
+++ js/app.js
@@ -1,27 +1,25 @@
-// TODO: initial locations array
-
 var initialLocations = [{
-		name: "Weston Homes Community Stadium",
+		name: 'Weston Homes Community Stadium',
 		lat: 51.9236601,
 		lng: 0.8968440999999998
 	},
 	{
-		name: "Tesco Highwoods",
+		name: 'Tesco Highwoods',
 		lat: 51.9098917,
 		lng: 0.9188577999999998
 	},
 	{
-		name: "High Woods Country Park",
+		name: 'High Woods Country Park',
 		lat: 51.9071739,
 		lng: 0.9100735000000002
 	},
 	{
-		name: "Hamiltons Gym",
+		name: 'Hamiltons Gym',
 		lat: 51.91989160000001,
 		lng: 0.9304106999999999
 	},
 	{
-		name: "The Espresso Room - Colchester",
+		name: 'The Espresso Room - Colchester',
 		lat: 51.8893328,
 		lng: 0.9005054
 	},
@@ -35,15 +33,15 @@ var Location = function(data) {
 		lng: data.lng
 	};
 
-	self.content = "<div>" + self.name + "<hr> Couldn't retrieve info" + "<div>";
-	var foursquareSearchURL = "https://api.foursquare.com/v2/venues/search?" +
-		"ll=" + this.position.lat + ',' + this.position.lng +
-		"&query=" + this.name +
-		"&intent=browse" +
-		"&radius=500" +
-		"&client_id=KGEL5SRKAFDQJZRQU3AW505SCRY3LSEYWYEQ2QB1AFAFJJP1" +
-		"&client_secret=NG4ISOMGM4TWPESFNGHU13JJBMECMHAQFFKSX20W10EX0NNN&" +
-		"v=20170622";
+	self.content = '<div>' + self.name + '<hr> Couldn\'t retrieve info' + '<div>';
+	var foursquareSearchURL = 'https://api.foursquare.com/v2/venues/search?' +
+		'll=' + this.position.lat + ',' + this.position.lng +
+		'&query=' + this.name +
+		'&intent=browse' +
+		'&radius=500' +
+		'&client_id=KGEL5SRKAFDQJZRQU3AW505SCRY3LSEYWYEQ2QB1AFAFJJP1' +
+		'&client_secret=NG4ISOMGM4TWPESFNGHU13JJBMECMHAQFFKSX20W10EX0NNN&' +
+		'v=20170622';
 
 
 	// TODO: .fail()/ERROR HANDLING
@@ -53,50 +51,50 @@ var Location = function(data) {
 			venueID = data.response.venues[0].id;
 			getVenueInfo(venueID);
 		} else {
-			self.content += "</div>"
+			self.content += '</div>'
 			showInfowindow();
 		}
 	}).fail(function() {
-		self.content += "</div>";
+		self.content += '</div>';
 		showInfowindow();
 	});
 
 	var getVenueInfo = function(venueID) {
-		var foursquareVenueURL = "https://api.foursquare.com/v2/venues/" + venueID +
-			"?client_id=KGEL5SRKAFDQJZRQU3AW505SCRY3LSEYWYEQ2QB1AFAFJJP1" +
-			"&client_secret=NG4ISOMGM4TWPESFNGHU13JJBMECMHAQFFKSX20W10EX0NNN&" +
-			"v=20170622";
+		var foursquareVenueURL = 'https://api.foursquare.com/v2/venues/' + venueID +
+			'?client_id=KGEL5SRKAFDQJZRQU3AW505SCRY3LSEYWYEQ2QB1AFAFJJP1' +
+			'&client_secret=NG4ISOMGM4TWPESFNGHU13JJBMECMHAQFFKSX20W10EX0NNN&' +
+			'v=20170622';
 		$.getJSON(foursquareVenueURL, function(data) {
 			data = data.response.venue;
-			self.content = "<div>" + self.name + "<hr>";
+			self.content = '<div>' + self.name + '<hr>';
 			if (data.contact.formattedPhone) {
 				var phone = data.contact.formattedPhone;
-				self.content += "Phone: " + phone;
+				self.content += 'Phone: ' + phone;
 			}
 			if (data.location.formattedAddress) {
 				var address = data.location.formattedAddress;
-				self.content += "<br>Address: " + address;
+				self.content += '<br>Address: ' + address;
 			}
 			if (data.categories[0].name) {
 				var category = data.categories[0].name;
-				self.content += "<br>Category: " + category;
+				self.content += '<br>Category: ' + category;
 			}
 			if (data.url) {
 				var url = data.url;
-				self.content += "<br>URL: <a href=\"" + url + "\" target=\"_blank\">" + url + "</a>";
+				self.content += '<br>URL: <a href="' + url + '" target="_blank">' + url + '</a>';
 			}
 			if (data.description) {
 				var desc = data.description;
-				self.content += "<br>Description: " + desc;
+				self.content += '<br>Description: ' + desc;
 			}
 			if (data.photos.count > 0) {
 				var photo = data.photos.groups[0].items[0];
-				var photoURL = photo.prefix + photo.width + "x" + photo.height + photo.suffix;
-				self.content += "<br><img src=\"" + photoURL + "\">";
+				var photoURL = photo.prefix + photo.width + 'x' + photo.height + photo.suffix;
+				self.content += '<br><img src="' + photoURL + '">';
 			}
-			self.content += "</div>"
+			self.content += '</div>'
 		}).fail(function() {
-			self.content += "</div>";
+			self.content += '</div>';
 			showInfowindow();
 		});
 	};
@@ -114,14 +112,19 @@ var Location = function(data) {
 		}
 	});
 	this.marker.addListener('click', function() {
-		activeMarker(self.marker);
-		self.showInfowindow();
+		if (activeMarker() == self.marker) {
+			activeMarker(null);
+			infowindow.setMap(null);
+		} else {
+			self.showInfowindow();
+		}
 	});
 
 	this.showInfowindow = function() {
 		if (infowindow.marker != self.marker) {
 			infowindow.marker = self.marker;
 			infowindow.setContent(self.content);
+			activeMarker(self.marker);
 			infowindow.open(map, self.marker);
 		}
 	};
@@ -142,7 +145,7 @@ var ViewModel = function() {
 	infowindow = new google.maps.InfoWindow({
 		content: ''
 	});
-	activeMarker = ko.observable("");
+	activeMarker = ko.observable('');
 	infowindow.addListener('closeclick', function() {
 		this.marker = null;
 	});
